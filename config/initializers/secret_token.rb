2b5e12066bc92d7fc980066bc8f52b08a3506530
A. Felipe Cabargas Madrid <felipe.cabargas@gmail.com>
2013-07-23 14:30:49 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pam::Application.config.secret_token = '3f46a403492d324202e3ed39f43af0c0d5ff563fd65d8c8f0e0bf8e61a79ec72cedbc483b23aaa4c8058502f0276f509414721aa75a577fafa7a0b5ab2f4bc5b'
