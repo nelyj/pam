3911e7b8fe28100aef92a1ccb8c8cb1346568fc4
Nelson Jiménez <developer.jimenez@gmail.com>
2013-10-22 16:57:14 -0300

    Se hacen cambios en el menu para que quede active cuando el controlador sea el mismo independiente de la página

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
 	def is_active?(link_path)
-		if current_page?(link_path)
+		if url_for(link_path).include? params[:controller] or current_page?(link_path)
 			"active"
 		else
 			""
 		end
 	end
+
 end
